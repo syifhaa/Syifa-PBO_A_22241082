3f8306b311e09e5ef5853fabc18a02b0728a5dc5
syifhaa <146147657+syifhaa@users.noreply.github.com>
2023-11-06 14:43:35 +0800

    Add files via upload

---
+++ 00_Template/src/com/tutorial/Main.java
@@ -0,0 +1,32 @@
+package com.tutorial;
+
+
+class Mahasiswa {
+    String Nama;
+    String Nim;
+    String Prodi;
+}
+
+public class Main {
+
+    public static void main(String[] args){
+        // instansiasi pembuatan objek
+        Mahasiswa mhs1 = new Mahasiswa();
+
+        // objek mhs1 diberi atribut
+        mhs1.Nama = "sifa";
+        mhs1.Nim = "22241082";
+        mhs1.Prodi = "Teknologi Informasi";
+
+        System.out.println("Nama Mahasiswa:" + mhs1.Nama);
+        System.out.println("Nim Mahasiswa:" + mhs1.Nim);
+        System.out.println("Prodi Mahasiswa:" + mhs1.Prodi);
+
+
+        
+    }
+    
+    
+}
+
+
---
+++ 01_pengenalan class dan objek/src/com/tutorial/Main.java
@@ -0,0 +1,41 @@
+package com.tutorial;
+
+
+class Mahasiswa {
+    String Nama;
+    String Nim;
+    String Prodi;
+}
+
+public class Main {
+
+    public static void main(String[] args){
+        // instansiasi pembuatan objek
+        Mahasiswa mhs1 = new Mahasiswa();
+
+        // objek mhs1 diberi atribut
+        mhs1.Nama = "sifa";
+        mhs1.Nim = "22241082";
+        mhs1.Prodi = "Teknologi Informasi";
+
+        System.out.println("Nama Mahasiswa:" + mhs1.Nama);
+        System.out.println("Nim Mahasiswa:" + mhs1.Nim);
+        System.out.println("Prodi Mahasiswa:" + mhs1.Prodi);
+
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.Nama = "sifa";
+        mhs2.Nim = "22241082";
+        mhs2.Prodi = "Teknologi Informasi";
+
+        System.out.println("Nama Mahasiswa:" + mhs2.Nama);
+        System.out.println("Nim Mahasiswa:" + mhs2.Nim);
+        System.out.println("Prodi Mahasiswa:" + mhs2.Prodi);
+
+
+        
+    }
+    
+    
+}
+
+
